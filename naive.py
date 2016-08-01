f769a334f8f7520cf5e653ee61e50e8aed0a7e77
Kumar Ayush <cheekujodhpur@gmail.com>
2016-08-01 09:08:57 +0530

    basic equations done, simulates for fixed looking direction, no image transformation yet

---
+++ naive.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+
+# Number of simulation points
+N = 1000
+
+# Constants
+rho = 0.5
+
+# Functions
+def nl(theta, phi):
+    return -np.sin(theta)*np.cos(phi)
+
+def nph(theta, phi):
+    return -np.sin(theta)*np.sin(phi)
+
+def nt(theta, phi):
+    return np.cos(theta)
+
+def r(l):
+    return np.sqrt(rho*rho + l*l)
+
+def dr_dl(l):
+    return l/r(l)
+
+def p_l(theta, phi):
+    return nl(theta, phi)
+
+def p_theta(r,theta,phi):
+    return r*nt(theta, phi)
+
+def p_phi(r, theta, phi, theta_c):
+    return r * np.sin(theta_c) * nph(theta, phi)
+
+def b(r, theta, phi, theta_c):
+    return p_phi(r, theta, phi, theta_c)
+
+def B_sq(r, theta, phi):
+    M = nt(theta, phi)
+    N = nph(theta, phi)
+    return r*r*(M*M + N*N)
+
+ct_b = 1
+ct_Bsq = 1
+
+# Time for simulation
+T = 100
+t = np.linspace(0, T, N)
+
+def dy_dt(y, t):
+    return np.array([
+            y[3],
+            y[4]/(r(y[0])**2),
+            ct_b/((r(y[0])**2)*(np.sin(y[1])**2)),
+            ct_Bsq*((dr_dl(y[0]))/(r(y[0])**3)),
+            ((ct_b**2)/(r(y[0])**2))*(np.cos(y[1])/(np.sin(y[1])**3))
+            ])
+
+
+# Trial simulation
+theta_cs = np.pi/4
+phi_cs = np.pi/4
+
+# Initial
+l = 4.0
+theta = np.pi/2
+phi = 0
+
+ct_b = b(r(l), theta_cs, phi_cs, theta)
+ct_Bsq = B_sq(r(l), theta_cs, phi_cs)
+
+y0 = np.array([
+        l,
+        theta,
+        phi,
+        p_l(theta_cs, phi_cs),
+        p_theta(r(l), theta_cs, phi_cs)
+        ])
+
+
+print "Entering simulation"
+result = odeint(dy_dt, y0, t)
+print "Simulation done"
+
+print result[-1]
